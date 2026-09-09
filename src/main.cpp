adb803ae554fd211c96ae61d0ca64f7b767819f6
Jason89475 <jasonc3n@gmail.com>
2026-09-09 19:22:51 -0400

    Task1 for iotLab3

---
+++ src/main.cpp
@@ -0,0 +1,71 @@
+#include <Arduino.h>  
+
+ 
+
+uint8_t value; 
+
+ 
+
+void setup(){ 
+
+  // configure PD7_PD4 as inputs(0x0F) 
+
+  DDRD = DDRD & 0x0F; 
+
+ 
+
+  //enable input pullup (0xF0) 
+
+  PORTD |= 0xF0; 
+
+ 
+
+  //configure PB3_PB0 (0x0F) 
+
+  DDRB = DDRB & 0x0F; 
+
+ 
+
+ Serial.begin (9600); 
+
+} 
+
+ 
+
+void loop(){ 
+
+  //Read switches 
+
+  value = PIND; 
+
+  //mask the values(0xF0) 
+
+  value = value & 0xF0; 
+
+  // Shift right value >>4 
+
+  value = value >> 4; 
+
+  //invert the values 
+
+  value = ~value; 
+
+  //mask value(0x0F) 
+
+  value = value & 0x0F; 
+
+  //Display in LED 
+
+  PORTB = value; 
+
+  Serial.print("Binary:"); 
+
+  Serial.println(value,BIN); 
+
+  Serial.print("Decimal:"); 
+
+  Serial.println(value); 
+
+  delay(1000); 
+
+} 
